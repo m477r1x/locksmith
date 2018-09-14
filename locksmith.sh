f4ad1429fa99a14bece7d14912da102767925fbf
Love <mlove@mc109509.ni.ad.newsint>
2018-09-14 15:56:34 +0100

    re-engineered new credential storage for testing

--- locksmith.sh
+++ locksmith.sh
@@ -6,18 +6,22 @@ for account in $(cat ~/.aws/credentials | grep '\[.*\]' |  tr -d '[]'); do
 OldKeyID=$(aws iam list-access-keys --user $user --query "AccessKeyMetadata[?CreateDate<='$(date +%F),AccessKeyId']" --output text --profile $account | awk '{print $1}');
 aws sts get-caller-identity --profile $account --output text | awk '{print "Current Access Key "$3" for account: '$account' is valid, beginning rotation"}'
 if [ $? -eq 0 ]; then
-	aws iam create-access-key --user-name $user --output text --profile $account | awk '{print "['$account']\n""aws_access_key_id = "$2"\n""aws_secret_access_key = "$4"\n"}' >> /tmp/credentials
+	newcreds=$(aws iam create-access-key --user-name $user --output text --profile $account | awk '{print$2" "$4}')
+	newaccesskey=$(echo $newcreds | awk '{print$1}')
+	newsecretkey=$(echo $newcreds | awk '{print$2}')
+	printf [account]"\n"$newaccesskey"\n"$newsecretkey"\n""\n" > /tmp/credentials
 		if [ $? -eq 0 ]; then
 			echo "Successfully created new key in "$account
 		else
 			tput bold
-			echo "Failed to create new key in "$account" probably too many keys present, or must be logged in using MFA"
+			echo "Failed to create new key in "$account" too many keys, or must be logged in using MFA"
 			tput sgr0
 		fi;
 	echo "Waiting to ensure new key is active..."
 	sleep 5
 	echo "Testing new access key for account "$account;
-	aws sts get-caller-identity;
+	
+	AWS_ACCES_KEY_ID=$newaccesskey AWS_SECRET_ACCESS_KEY=$newsecretkey aws sts get-caller-identity;
 		if [ $? -eq 0 ]; then
 			echo "Successful"
 		else 
